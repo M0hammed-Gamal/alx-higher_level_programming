0afa22ac505328cbbda522afed4056b8aded7a3c
M0hammed-Gamal <m_m20000075@yahoo.com>
2023-09-26 03:13:50 -0700

    python

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,53 +1,50 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+Defaults size to 0. Raise error on invalid size inputs.
+Methods Getter and Setter properties for size.
+Method area returns size of area of the square.
+Methods __lt__, __le__, __eq__, __ne__, __gt__, __ge__ allow
+comparisons between Square objects and their sizes.
+"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
-    def __eq__(self, other):
-        return self.__size == other.__size
-
-    def __lt__(self, other):
-        return self.__size < other.__size
-
-    def __le__(self, other):
-        return self.__size <= other.__size
-
-    def __ne__(self, other):
-        return self.__size != other.__size
-
-    def __gt__(self, other):
-        return self.__size > other.__size
-
-    def __ge__(self, other):
-        return self.__size >= other.__size
-
+    """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def area(self):
-        """ Method that returns the square are of the object
-        """
-        return (self.__size ** 2)
+        self.size = size
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """ Method to set the size value of the square object
-        """
-        if not isinstance(value, int):
+    def size(self, size):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __lt__(self, other):
+        return self.__size < other.size
+
+    def __le__(self, other):
+        return self.__size <= other.size
+
+    def __eq__(self, other):
+        return self.__size == other.size
+
+    def __ne__(self, other):
+        return self.__size != other.size
+
+    def __gt__(self, other):
+        return self.__size > other.size
+
+    def __ge__(self, other):
+        return self.__size >= other.size
