c585e97714be70d4f85c2e4d0031ed085f692dc4
M0hammed-Gamal <m_m20000075@yahoo.com>
2023-10-10 09:43:05 +0000

    hot fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
+""" Module that writes an Object to a text file using
+a JSON representation
 """
-    script that adds all arguments to a Python list,
-    and then save them to a file:
-
-    You must use your function save_to_json_file from
-    7-save_to_json_file.py
-    You must use your function load_from_json_file
-    from 8-load_from_json_file.py
-    The list must be saved as a JSON representation
-    in a file named add_item.json
-    If the file doesn’t exist, it should be created
-    You don’t need to manage file permissions / exceptions.
-"""
-
-
 import json
-import sys
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-arg = sys.argv[1:]
-file_name = "add_item.json"
 
-try:
-    python_object = load_from_json_file(file_name)
-except FileNotFoundError:
-    save_to_json_file([], file_name)
+def save_to_json_file(my_obj, filename):
+    """ Function that writes an object to a text file
+    by a JSON representation
 
-python_object = load_from_json_file(file_name)
-if type(python_object) is list:
-    for item in arg:
-        python_object.append(item)
+    Args:
+        my_obj: object
+        filename: textfile name
 
-save_to_json_file(python_object, file_name)
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
